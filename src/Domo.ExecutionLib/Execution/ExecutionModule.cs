d3acdaf7d1e64e63558bd4e440fc3995bdca9658
Stefan Domnanovits <stefan@codebullets.com>
2011-03-04 19:24:04 +0100

    Execution module skeleton.

---
+++ src/Domo.ExecutionLib/Execution/ExecutionModule.cs
@@ -0,0 +1,26 @@
+﻿namespace Domo.ExecutionLib.Execution
+{
+   using System;
+
+   /// <summary>
+   /// Relays incoming message to the execution queue.
+   /// </summary>
+   public class ExecutionModule : IExecutionModule
+   {
+      /// <summary>
+      /// Add a message to be handled.
+      /// </summary>
+      /// <param name="message">The message</param>
+      public void Add(IMessage message)
+      {
+      }
+
+      /// <summary>
+      /// Action to be executed.
+      /// </summary>
+      /// <param name="action">The action delegate to execute.</param>
+      public void Add(Action action)
+      {
+      }
+   }
+}
